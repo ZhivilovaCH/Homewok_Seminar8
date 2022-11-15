168ff74e47983b4d308755348b6fe5f0a4c52723
Christina Zhivilova <krishakirova@yandex.ru>
2022-11-15 17:00:48 +0300

    Добавление Task 54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,2 +1,73 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
 //элементы каждой строки двумерного массива. 
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetNimbersDescendingOrder(int[,] inArray)
+{
+  
+    int temp = 0;
+
+    for (int i = 0; i < inArray.GetLength(0) ; i++)
+    {
+
+        for (int j = 0; j < inArray.GetLength(1) - 1; j++)
+        {
+            for (int p = j+1; p < inArray.GetLength(1); p++)
+        {
+
+            if (inArray[i, j] < inArray[i, p])
+            {
+                temp = inArray[i, j];
+                inArray[i, j] = inArray[i, p];
+                inArray[i,p] = temp;
+            }
+
+        }
+        }
+
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+
+}
+Console.WriteLine();
+int[,] array = GetArray(row, columns, 0, 10);
+PrintArray(array);
+Console.Write("******************* ");
+Console.WriteLine();
+Console.WriteLine("Элементы по убыванию ");
+GetNimbersDescendingOrder(array);
--- Task56/Program.cs
+++ Task56/Program.cs
@@ -1,2 +1,81 @@
 ﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CountMinSum(int[,] inArray)
+{
+    int minSumm = 0;
+    int maxSumm = 0;
+    int temp = 0;
+    int s = 0;
+    int n = 0;
+    while (n < row)
+    {
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+
+            for (int j = 0; j < inArray.GetLength(1); j++)
+            {
+                temp += inArray[n, j];
+            }
+         
+            Console.Write($"{("Сумма строки = " + temp)}\t ");
+
+            if (temp > maxSumm)
+                maxSumm = temp;
+
+            if (minSumm == 0)
+                minSumm = temp;
+
+            if (minSumm > temp)
+            {
+                minSumm = temp;
+                s = i;
+
+            }
+
+            temp = 0;
+            n++;
+        }
+        s+=1;
+        
+    }
+    Console.WriteLine();
+    Console.Write($"{("Строка с наименьшей суммой элементов = "+ s)}\t ");
+}
+
+
+int[,] array = GetArray(row, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+CountMinSum(array);
